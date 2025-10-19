ce9b0f86deef002b69ba1cd588959b5acb900654
tom-gordon13 <tombcgordon@gmail.com>
2025-10-19 15:33:53 -0600

    Update to cleaner server domain

--- ui/constants/config.ts
+++ ui/constants/config.ts
@@ -1,4 +1,4 @@
 export const config = {
   // Production server URL on Vercel
-  serverUrl: __DEV__ ? 'http://localhost:3000' : 'https://workout-analyzer-server-dd3ekbwyj-tom-gordon13s-projects.vercel.app',
+  serverUrl: __DEV__ ? 'http://localhost:3000' : 'https://workout-analyzer-server.vercel.app',
 };
